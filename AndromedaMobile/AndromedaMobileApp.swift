610945baae1ae6bde872860a1f322527428db339
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2023-09-05 17:41:06 +0200

    Initial Commit

---
+++ AndromedaMobile/AndromedaMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AndromedaMobileApp.swift
+//  AndromedaMobile
+//
+//  Created by Kamil Kuczaj on 05/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct AndromedaMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
