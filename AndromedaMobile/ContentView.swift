872e7d937321f0bc5f4343bfe1c94db388672570
Kamil Kuczaj <kamil.kuczaj@gmail.com>
2023-09-05 17:49:00 +0200

    Initial app for MVP testing.

--- AndromedaMobile/ContentView.swift
+++ AndromedaMobile/ContentView.swift
@@ -8,16 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var messageText: String = ""
+    @State private var messages: [(text: String, timestamp: String)] = []
+    @State private var darkMode: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                // Scrollable message area
+                List {
+                    ForEach(messages, id: \.timestamp) { msg in
+                        HStack {
+                            Text(msg.text)
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 12).fill(Color.blue))
+                                .foregroundColor(.white)
+                            
+                            Spacer()
+                            
+                            Text(msg.timestamp)
+                                .font(.footnote)
+                                .foregroundColor(.gray)
+                        }
+                    }
+                }
+                
+                // Text box & send button
+                HStack {
+                    TextField("Type your message...", text: $messageText)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Button("Send") {
+                        let timestamp = "\(Date())"
+                        messages.append((text: messageText, timestamp: timestamp))
+                        messageText = ""
+                    }
+                }
+                .padding()
+            }
+            .navigationBarItems(trailing:
+                HStack {
+                    // Dark mode toggle
+                    Toggle(isOn: $darkMode) {
+                        Text("Dark Mode")
+                    }
+                    .toggleStyle(SwitchToggleStyle(tint: .blue))
+                }
+            )
+            .preferredColorScheme(darkMode ? .dark : .light)
         }
-        .padding()
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
